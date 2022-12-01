1d3f1bfdcea5498fdfa9e970e38a3d81b31e85fc
Marina Chernykh <marina.u.chernykh@gmail.com>
2022-12-01 19:15:46 +0300

    all parts have been merged

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -20,6 +20,7 @@ router_v1.register(
     CommentViewSet, basename='comments'
     )
 
+
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
     path('v1/auth/signup/', SignUp.as_view(), name='signup'),
